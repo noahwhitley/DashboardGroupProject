b9d73031558e5cfb004187ff3eb685950bbb8e6f
NathanR34 <nathaneal.robinson3442@stu.mtec.edu>
2023-09-01 09:29:31 -0600

    changes Fri Sep  1 09:29:31 MDT 2023

--- nathans/graphs.js
+++ nathans/graphs.js
@@ -1,39 +1,58 @@
 
 
 
-function run(){
+function runGraphs(){
     
     function getMonthName(month){
         return ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][month%12];
+    }
+
+    function getLoadin(element){
+        let cloneElement;
+        if(element){
+            cloneElement = element.querySelector(".js-loadin-demo");
+        }
+        while(element.firstChild){
+            element.removeChild(element.firstChild);
+        }
+        if(cloneElement){
+            cloneElement = cloneElement.cloneNode(true);
+            cloneElement.classList.remove("js-loadin-demo");
+            return cloneElement;
+        }
+        return;
     }
     
     function createBarGraphs(data){
         // Designed to work on one element for now. 
         // Design may be changed in feature for more dynamic behavior
         let barsElm = document.querySelector("#finance-performance-graph .bars");
+        
         let cloneBar;
         if(barsElm){
-            cloneBar = barsElm.querySelector(".bar-element");
+            cloneBar = getLoadin(barsElm);
         }
         if(cloneBar){
             cloneBar = cloneBar.cloneNode(true);
-            while(barsElm.firstChild){
-                barsElm.removeChild(barsElm.firstChild);
-            }
-            
-            let bars = [];
+           
             let month = 0;
             let size = 0;
-    
             for(let barData of data){
 
-                if(barData.month != undefined){
+                if(typeof barData.size != "undefined"){
+                    size = barData.size;
+                }
+                barData.size = size;
+
+                if(typeof barData.month != "undefined"){
                     month = barData.month - 1;
                 } else {
                     month++;
                 }
-                if(barData.size != undefined){
-                    size = barData.size;
+                barData.month = month;
+
+                if(typeof barData.element == "undefined"){
+                    barData.element = {};
                 }
     
                 let bar = cloneBar.cloneNode(true);
@@ -44,49 +63,53 @@ function run(){
                 if(weekDay){
                     weekDay.innerText = getMonthName(month).toUpperCase().slice(0, 3);
                 }
+
                 let barGrow = bar.querySelector(".bar-grow");
                 if(barGrow){
                     barGrow.style.height = "0%";
+                    barData.element.barGrow = barGrow;
                 }
-                
-                barData.size = size;
-                barData.month = month;
-                barData.elm = barGrow;
             }
         }
         return data;
     }
 
-    function animateBars(bars, r){
-        async function runAnimation(bar, v){
-            let animation = bar.elm.animate([
-                {height:v+"%"}
-            ],{
-                duration: 3000,
-                interations: 1,
-                easing: "ease",
-                fill: "forwards"
-            });
+    async function runAnimation(elm, v){
+        let animation = elm.animate([
+            {height:v+"%"}
+        ],{
+            duration: 3000,
+            interations: 1,
+            easing: "ease",
+            fill: "forwards"
+        });
 
-            await animation.finished;
-            console.log("done")
-            animation.commitStyles();
-        }
+        await animation.finished;
+
+        animation.commitStyles();
+        animation.cancel();
+    }
+    
+    function animateBars(bars, r){
         for(let bar of bars){
-            if(!bar.elm) continue;
-            runAnimation(bar, Math.round(r * bar.size));
+            if(!bar.element.barGrow) continue;
+            runAnimation(bar.element.barGrow, Math.round(r * bar.size));
         }
     }
     
     let data = createBarGraphs([
-        {size: 40, month: 7},
-        {},
-        {size: 70},
-        {},
-        {size: 20},
-        {size: 98}
+        {size: 70, month: 7},
+        {size: 45},
+        {size: 38},
+        {size: 50},
+        {size: 75},
+        {size: 85}
     ]);
     animateBars(data, 1);
+
+    // for debug and use
+    window.runAnimation = runAnimation;
+    window.getLoadin = getLoadin;
 }
 
-run();
+runGraphs();
